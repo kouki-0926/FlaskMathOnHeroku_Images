9d4b3624d4ef38c27ec6c9411cae208667d22aeb
Terakado Koki <terakou37910@icloud.com>
2025-03-26 20:24:58 +0900

    update

--- castles/main.py
+++ castles/main.py
@@ -1,13 +1,29 @@
 import json
+import requests
+
+response = requests.get(
+    "https://raw.githubusercontent.com/kouki-0926/FlaskMathOnHeroku_Images/main/picture/image_info.json")
+image_info = response.json()
 
 castles_list = {}
 with open('castles/100castles.txt', 'r', encoding='utf-8') as f:
     for line in f:
         tmp = line.split("\t")
 
-        castles_list[tmp[1]] = {"location": tmp[2] + tmp[3],
-                                "coords": [tmp[4], tmp[5]],
-                                "photo": "https://raw.githubusercontent.com/kouki-0926/FlaskMathOnHeroku_Images/main/picture/" + tmp[2] + "/" + tmp[1] + ".jpg"}
+        tmp_list = []
+        for key in image_info.keys():
+            for marker in image_info[key]["markers"]:
+                if tmp[1] in marker["title"] and tmp[2] in key:
+                    tmp_list.append({"number": tmp[0],
+                                     "city": tmp[2] + tmp[3],
+                                     "photo": marker["photo"]})
+
+        if len(tmp_list) > 0:
+            castles_list[tmp[1]] = tmp_list
+        else:
+            castles_list[tmp[1]] = [{"number": tmp[0],
+                                     "city": tmp[2] + tmp[3],
+                                     "photo": "https://placehold.jp/ffffff/000000/300x300.jpg?text=未訪問"}]
 
 with open("castles/castles.json", "w", encoding='utf-8') as f:
     json.dump(castles_list, f, ensure_ascii=False, indent=4)
