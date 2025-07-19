a72851b13d3b509736abc21486d5448377b55c2d
Terakado Koki <terakou37910@icloud.com>
2025-07-19 23:34:03 +0900

    update

--- castles/main.py
+++ castles/main.py
@@ -5,23 +5,23 @@ response = requests.get("https://raw.githubusercontent.com/kouki-0926/FlaskMathO
 image_info = response.json()
 
 castles_list = {}
-with open('castles/100castles.txt', 'r', encoding='utf-8') as f:
+with open("castles/100castles.txt", "r", encoding="utf-8") as f:
     for line in f:
-        tmp = line.split("\t")
+        castle_info = line.split("\t")
 
         tmp_list = []
         for key in image_info.keys():
-            if tmp[2] in key:
+            if castle_info[2] in key:
                 for marker in image_info[key]["markers"]:
-                    if tmp[1] in marker["title"]:
-                        tmp_list.append({"city": tmp[2] + tmp[3],
+                    if castle_info[1] in marker["title"]:
+                        tmp_list.append({"city": castle_info[2] + castle_info[3],
                                          "photo": marker["photo"]})
 
         if len(tmp_list) > 0:
-            castles_list[tmp[1]] = tmp_list
+            castles_list[castle_info[1]] = tmp_list
         else:
-            castles_list[tmp[1]] = [{"city": tmp[2] + tmp[3],
-                                     "photo": "https://placehold.jp/ffffff/000000/400x300.jpg?text=未訪問"}]
+            castles_list[castle_info[1]] = [{"city": castle_info[2] + castle_info[3],
+                                             "photo": "https://placehold.jp/ffffff/000000/400x300.jpg?text=未訪問"}]
 
-with open("castles/castles.json", "w", encoding='utf-8') as f:
+with open("castles/castles.json", "w", encoding="utf-8") as f:
     json.dump(castles_list, f, ensure_ascii=False, indent=4)
